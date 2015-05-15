723f6be31bce6d66634d6e1e4903a041614256b8
claireyoung <clairez@gmail.com>
2015-05-15 11:07:53 -0700

    Android Studio 1.2 compatibility
    Android Studio 1.2 compatibility requires Gradle upgrade. Also,
    MyActivity should extend from Activity, since ActionBarActivity now
    requires AppCompat theme.

--- app/src/main/java/io/keen/client/android/example/MyActivity.java
+++ app/src/main/java/io/keen/client/android/example/MyActivity.java
@@ -14,7 +14,9 @@ import io.keen.client.java.KeenClient;
 import io.keen.client.java.KeenLogging;
 import io.keen.client.java.KeenProject;
 
-public class MyActivity extends ActionBarActivity {
+import android.app.Activity;
+
+public class MyActivity extends Activity {
 
     private static int clickNumber = 0;
 
